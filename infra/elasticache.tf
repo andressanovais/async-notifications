7a9dce46c91ed0071057c348c5e862bb44f356f1
andressanovais <dressanovais@gmail.com>
2023-09-01 01:50:22 -0300

    Adicionando SecretsManager com senha do Redis

--- infra/elasticache.tf
+++ infra/elasticache.tf
@@ -1,4 +1,4 @@
-/* So possui implementacao local no LocalStack Pro, por isso estou comentando
+/* So possui implementacao local no LocalStack Pro
 resource "aws_elasticache_replication_group" "web-notifications-cache" {
   replication_group_id = "connections-web-notification"
 
@@ -9,10 +9,28 @@ resource "aws_elasticache_replication_group" "web-notifications-cache" {
   num_cache_clusters         = 2
   automatic_failover_enabled = true
 
+  at_rest_encryption_enabled = true
+  transit_encryption_enabled = true
+  auth_token = random_password.password.result
 }
 
 resource "aws_elasticache_subnet_group" "cache-subnets" {
   name       = "web-notifications-subnets"
   subnet_ids = var.subnet_ids
 }
- */
+*/
+
+ resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "_%@"
+}
+  
+resource "aws_secretsmanager_secret" "redis_secret" {
+   name = "async-notifications/redis-password"
+}
+
+resource "aws_secretsmanager_secret_version" "sversion" {
+  secret_id = aws_secretsmanager_secret.redis_password.id
+  secret_string = random_password.password.result
+}
